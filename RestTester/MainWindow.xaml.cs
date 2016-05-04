d096964f9e3fcfdd860294cb91e6ec093c9f1126
Andrew Weeks <adweeks@gmail.com>
2016-05-03 20:15:07 -0500

    fixed themes to load on open by creating themeWindow on load

--- RestTester/MainWindow.xaml.cs
+++ RestTester/MainWindow.xaml.cs
@@ -19,6 +19,7 @@ namespace RestTester
         HeadersWindow headersWindow;
         public MainWindow()
         {
+            themeWindow = new ThemeWindow();
             vm = new ViewModel.MainViewModel();
             this.DataContext = vm;
             
